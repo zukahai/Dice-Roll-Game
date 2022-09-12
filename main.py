2dbfc9986616178233ea8c0083f7b3cb586f65b2
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2022-09-13 03:17:34 +0700

    Update images

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pygame, random
  
 pygame.init()
-screen = pygame.display.set_mode((600, 450))
+game_W = 600
+game_H = 450
+N_dice = 3
+screen = pygame.display.set_mode((game_W, game_H))
 done = False
 clock = pygame.time.Clock()
 x = 30
@@ -15,11 +18,16 @@ def pickNumber():
     diceroll = random.randint(1, 6)
     nameImage = ['one', 'two', 'three', 'four', 'five', 'six']
     dice = pygame.image.load('images/' + nameImage[diceroll - 1] + ".png")
-         
-    return (dice, diceroll)
+    return {'dice': dice, 'diceroll': diceroll}
+
+roll = [0] * N_dice
+
+def initDice():
+    for i in range(0, N_dice):
+        roll[i] = pickNumber()
  
-dice, diceroll = pickNumber()
-print(dice.get_size()[0])
+initDice()
+# print(roll['dice'].get_size()[0])
 while done == False:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -29,11 +37,12 @@ while done == False:
         mouse_pos = pygame.mouse.get_pos()
              
         if button.collidepoint(mouse_pos):
-            dice, diceroll = pickNumber()
-            screen.blit(dice, (0, 0))
-            print(diceroll)
+            initDice()
+            print ([e['diceroll'] for e in roll])
+    
             
-    screen.blit(dice, (0, 0))
+    for i in range(0, N_dice):
+        screen.blit(roll[i]['dice'], (i * 200, 0))
  
     pygame.draw.rect(screen, BLACK, button)
          
