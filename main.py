97edfd89d1ac96d098fb1d22c52d35af38c538d6
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2022-09-13 02:27:00 +0700

    Init game

---
+++ main.py
@@ -0,0 +1,47 @@
+import pygame, random
+ 
+pygame.init()
+screen = pygame.display.set_mode((600, 450))
+done = False
+clock = pygame.time.Clock()
+x = 30
+y = 30
+ 
+BLACK = 0, 0, 0
+ 
+button = pygame.Rect(150, 150, 120, 80)
+ 
+def pickNumber():
+    diceroll = random.randint(1, 6)
+    nameImage = ['one', 'two', 'three', 'four', 'five', 'six']
+    dice = pygame.image.load(nameImage[diceroll - 1] + ".png")
+         
+    return (dice, diceroll)
+ 
+dice, diceroll = pickNumber()
+while done == False:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+ 
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        mouse_pos = pygame.mouse.get_pos()
+             
+        if button.collidepoint(mouse_pos):
+            dice, diceroll = pickNumber()
+            screen.blit(dice, (0, 0))
+            print(diceroll)
+            
+    screen.blit(dice, (0, 0))
+ 
+    pygame.draw.rect(screen, BLACK, button)
+         
+ 
+    pygame.display.flip()
+ 
+    screen.fill((255, 255, 255))
+     
+ 
+ 
+ 
+pygame.quit()
